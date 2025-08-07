3f37508addb13eca66c2abb7382385cdb1f2cd1b
ptrragrr <anggraeniputry08@gmail.com>
2025-08-07 09:04:12 +0700

    tralalelo tralala

--- app/AuthStack/LoginScreen.tsx
+++ app/AuthStack/LoginScreen.tsx
@@ -39,7 +39,7 @@ export default function LoginScreen() {
 
     try {
       const response = await axios.post(
-        ' https://fadf50ca5131.ngrok-free.app/api/auth/login',
+        'https://89d8fc5c282c.ngrok-free.app/api/auth/login',
         { email: username, password }
       );
 
